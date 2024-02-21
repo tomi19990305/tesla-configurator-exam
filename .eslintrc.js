3e426903a84068d014236fe21773bbfa31f51da1
Tamás Tóth <toth.tamas@alerant.hu>
2024-02-21 13:49:58 +0100

    Format initial code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'angular'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:angular/johnpapa',
-    "plugin:prettier/recommended"
-  ],
-  rules: {
-    // Additional rules or overrides can be added here
-  },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'angular'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:angular/johnpapa',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        // Additional rules or overrides can be added here
+    },
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
-import {Component} from '@angular/core';
-import {AsyncPipe, JsonPipe} from '@angular/common';
+import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [AsyncPipe, JsonPipe],
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-  `,
+    selector: 'app-root',
+    standalone: true,
+    imports: [AsyncPipe, JsonPipe],
+    template: ` <h1>Hello from {{ name }}!</h1> `,
 })
 export class AppComponent {
-  name = 'Angular';
-
+    name = 'Angular';
 }
